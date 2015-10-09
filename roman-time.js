03e88465cd5038fd42bf6d1392dc788a9804cc9a
Alex Dubikovsky <lokomilan94@gmail.com>
2015-10-09 17:23:23 +0500

    Add modified algorithm of checking values

--- roman-time.js
+++ roman-time.js
@@ -1,39 +1,31 @@
-function satisfies(number, max) {
-    return parseInt(number) == number && number >= 0 && number < max;
+var helpMessage = 'Run roman-time.js [0 <= hours < 24] [0 <= minutes < 60]';
+var messages = [helpMessage, 'Wrong time! ', 'Invalid arguments! '];
+var bases = [1, 5, 10, 50];
+var digits = ['I', 'V', 'X', 'L'];
+
+function checkNumber(supposedNumber, maxValue) {
+    return /^\d+$/.test(Number(supposedNumber)) && supposedNumber < maxValue;
 }
 
-var messages = ['OK', 'Wrong time! ', 'Invalid arguments! '];
-
-function failAndExit(errorCode) {
-    console.log(messages[errorCode] + 'Try roman-time.js -h to get the hint');
-    process.exit(errorCode);
-}
-
-function printHelp() {
-    console.log('Run roman-time.js -h to get this hint');
-    console.log('Run roman-time.js [hours] [minutes] to get roman time');
-    console.log('Hours and minutes should not be negative');
-    console.log('Hours should not exceed 23, minutes should not exceed 59');
+function getErrorCode(hours, minutes) {
+    if (hours === '-h') {
+        return 0;
+    } else {
+        return isNaN(hours) || isNaN(minutes) ? 2 : 1;
+    }
 }
 
 function makeRoman(number) {
     var currentNumber = Number(number);
     var romanString = '';
-    var bases = [1, 5, 10, 50];
-    var romanDigits = ['I', 'V', 'X', 'L'];
     for (var i = bases.length - 1; i >= 0; i--) {
-        var base = Number(bases[i]);
-        var digit = romanDigits[i];
         var preBase = Number(bases[i - 2 + i % 2]);
-        var preDigit = romanDigits[i - 2 + i % 2];
-        var times = Math.floor(currentNumber / base);
-        for (var j = 0; j < times; j++) {
-            romanString += digit;
-            currentNumber -= base;
-        }
-        if (currentNumber > 0 && base - currentNumber <= preBase) {
-            romanString = romanString + preDigit + digit;
-            currentNumber = currentNumber + preBase - base;
+        var preDigit = digits[i - 2 + i % 2];
+        romanString += digits[i].repeat(Math.floor(currentNumber / bases[i]));
+        currentNumber %= bases[i];
+        if (currentNumber > 0 && bases[i] - currentNumber <= preBase) {
+            romanString = romanString + preDigit + digits[i];
+            currentNumber = currentNumber + preBase - bases[i];
         }
     }
     if (romanString === '') {
@@ -45,16 +37,8 @@ function makeRoman(number) {
 var hours = process.argv[2];
 var minutes = process.argv[3];
 
-if (!(isNaN(hours) || isNaN(minutes))) {
-    if (satisfies(hours, 24) && satisfies(minutes, 60)) {
-        console.log(makeRoman(hours) + ':' + makeRoman(minutes));
-    } else {
-        failAndExit(1);
-    }
+if (checkNumber(hours, 24) && checkNumber(minutes, 60)) {
+    console.log(makeRoman(hours) + ':' + makeRoman(minutes));
 } else {
-    if (hours == '-h') {
-        printHelp();
-    } else {
-        failAndExit(2);
-    }
+    console.log(messages[getErrorCode(hours, minutes)]);
 }
